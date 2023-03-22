4b10d312966a850fb87095022541b9fe39403b20
SerginhoLeal <sergiojunioleal@hotmail.com>
2023-03-22 10:53:24 -0300

    new: experience in progress

---
+++ src/home/screens/experience/constants.ts
@@ -0,0 +1,40 @@
+export const graduation = [
+  {
+    time: 'Fev/2017 ~ Dez/2019',
+    graduation: 'High school - Cotemig',
+    what: 'Professional Technical Course',
+  },
+  {
+    time: 'Fev/2021 ~ 20~~',
+    graduation: 'College - Cotemig',
+    what: 'Systems Analysis and Development - Baccalaureate',
+  },
+  {
+    time: 'Set/2022 ~ 20~~',
+    graduation: 'Rocketseat - Course',
+    what: 'Technology Education Platform',
+  },
+  {
+    time: 'Set/2022 ~ 20~~',
+    graduation: 'KNN Idiomas - English',
+    what: 'English Course',
+  }
+];
+
+export const business = [
+  {
+    time: 'Out/2020 ~ 20~~',
+    business: 'Compass.UOL',
+    what: '...',
+  },
+  {
+    time: 'Ago/2021 ~ Mai/2022',
+    business: 'Terra Investimentos',
+    what: 'Junior Web Developer with React',
+  },
+  {
+    time: 'Jun/2022 ~ Dez/2022',
+    business: 'Maqcampo',
+    what: 'Junior Mobile Developer with React Native',
+  },
+];
