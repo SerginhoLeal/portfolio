b733103ce2a3c9dce0e0b8371382309e7a87f0b8
SerginhoLeal <sergiojunioleal@hotmail.com>
2023-03-22 10:58:55 -0300

    [new] my skills in progress

---
+++ src/common/modal/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import * as Styles from './styles';
+
+export const Modal: React.FC = () => {
+  return (
+    <Styles.Container>
+      <Styles.Content>
+        
+      </Styles.Content>
+    </Styles.Container>
+  );
+}
