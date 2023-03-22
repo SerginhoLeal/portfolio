866bc5f41c66f665c671fabbcbe4cbb15b8a4b31
SerginhoLeal <sergiojunioleal@hotmail.com>
2023-03-22 11:03:10 -0300

    [remove]

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,10 @@ import ReactDOM from 'react-dom/client'
 import App from './home'
 import './index.css';
 
-// import './assets/fonts/Poppins-Bold.ttf';
-// import './assets/fonts/Poppins-Light.ttf';
-
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 )
+
+//NB014289171BR
