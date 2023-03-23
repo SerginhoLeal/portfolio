817c6fcacf089277e65752b1c637140ea142f8cc
SerginhoLeal <sergiojunioleal@hotmail.com>
2023-03-23 10:49:25 -0300

    [test] in progress

--- src/home/styles.ts
+++
@@ -1,6 +0,0 @@
-import styled, { css } from 'styled-components';
-import Wallpaper from "../assets/img/wallpaper2.png";
-
-/* background-image: url(${Wallpaper});
-background-repeat:no-repeat;
-background-position:center; */
