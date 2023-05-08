3a919266d779f0da8cd0f619539214afbf02bdc3
SerginhoLeal <sergiojunioleal@hotmail.com>
2023-05-07 21:50:56 -0300

    svg

--- src/assets/svg/index.tsx
+++ src/assets/svg/index.tsx
@@ -1,17 +1,31 @@
-import { ReactComponent as Monitor } from './monitor.svg';
-import { ReactComponent as Brush } from './brush.svg';
 import { ReactComponent as Logo } from './logo.svg';
+import { ReactComponent as Terra } from './terra.svg';
+import { ReactComponent as Brush } from './brush.svg';
+import { ReactComponent as Github } from './github.svg';
 import { ReactComponent as Mobile } from './mobile.svg';
 import { ReactComponent as Driver } from './driver.svg';
-import { ReactComponent as Graduation } from './graduation.svg';
+import { ReactComponent as Discord } from './discord.svg';
+import { ReactComponent as Monitor } from './monitor.svg';
+import { ReactComponent as Linkedin } from './linkedin.svg';
 import { ReactComponent as Business } from './business.svg';
+import { ReactComponent as WhatsApp } from './whatsapp.svg';
+import { ReactComponent as Instagram } from './instagram.svg';
+import { ReactComponent as Graduation } from './graduation.svg';
+import { ReactComponent as IgniteShopLogo } from './ignite-shop-logo.svg';
 
 export {
-  Monitor,
-  Brush,
-  Mobile,
   Logo,
+  Brush,
+  Terra,
+  Mobile,
+  Github,
   Driver,
+  Monitor,
+  Discord,
+  Linkedin,
+  Business,
+  WhatsApp,
+  Instagram,
   Graduation,
-  Business
+  IgniteShopLogo
 };
